56b4951d072add8c114a419f323365614e5559af
addo <addo@itu.dk>
2021-12-15 12:54:40 +0100

    Template

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,8 @@
+import Controller.Controller;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Starting program...");
+        Controller.main(args);
+    }
+}
