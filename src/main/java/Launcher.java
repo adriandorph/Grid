a7ae04f7c54010bbc0637fe64f18b55ffa32d669
adriandorph <32704186+adriandorph@users.noreply.github.com>
2022-01-21 14:35:56 +0100

    change git user on desktop test

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,6 +3,7 @@ import Controller.Controller;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Starting program...");
+        System.out.println("test");
         Controller.main(args);
     }
 }
