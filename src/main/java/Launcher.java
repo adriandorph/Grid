c1332307afb213274af7fc226034dea41d8107ae
Adrian Bay Dorph <32704186+adriandorph@users.noreply.github.com>
2022-01-28 17:08:37 +0100

    removed console startup message

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,7 +2,6 @@ import Controller.Controller;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Starting...");
         Controller.main(args);
     }
 }
