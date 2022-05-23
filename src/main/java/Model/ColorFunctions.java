15be9f44d536f7b91f1922d488d6388b3c45bf8d
Adrian Bay Dorph <32704186+adriandorph@users.noreply.github.com>
2022-05-24 00:33:54 +0200

    refactored opacityOnBackGround in ColorFunctions

--- src/main/java/Model/ColorFunctions.java
+++ src/main/java/Model/ColorFunctions.java
@@ -5,9 +5,15 @@ import javafx.scene.paint.Color;
 public class ColorFunctions {
 
     public static Color opacityOnBackground(Color subject, Color background, double opacity){
-        double red = (subject.getRed() - background.getRed()) * opacity + background.getRed();
-        double green = (subject.getGreen() - background.getGreen()) * opacity + background.getGreen();
-        double blue = (subject.getBlue() - background.getBlue()) * opacity + background.getBlue();
+        double red = monoOpacityOnBackground(subject.getRed(), background.getRed(), opacity);
+        double green = monoOpacityOnBackground(subject.getGreen(), background.getGreen(), opacity);
+        double blue = monoOpacityOnBackground(subject.getBlue(), background.getBlue(), opacity);
         return Color.color(red, green, blue);
+    }
+
+    private static double monoOpacityOnBackground(double subject, double background, double opacity){
+        if (subject > 1.0 || subject < 0.0 || background > 1.0 || background < 0.0 || opacity > 1.0 || opacity < 0.0)
+            throw new RuntimeException("Bad input in monoOpacityOnBackground. subject: "+ subject + " background: " + background + " opacity: "+ opacity);
+        return (subject - background) * opacity + background;
     }
 }
